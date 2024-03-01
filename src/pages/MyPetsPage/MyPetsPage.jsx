59d635ba508a56d3c6bba64282941b9aa5940f7b
Marie S <marieslo@Maries.local>
2024-03-01 23:37:41 +0200

    improved responsive design

--- src/pages/MyPetsPage/MyPetsPage.jsx
+++ src/pages/MyPetsPage/MyPetsPage.jsx
@@ -73,17 +73,13 @@ export default function MyPetsPage() {
           onLike={likePet}
           onUnlike={unlikePet}
         />
-
-        {!(likedPetsUpdated || adoptedPetsUpdated || fosteredPetsUpdated) && (
-          <div className='they-need-your-love'>
-            <p>
-              For now, you don't have any saved, adopted, or fostered pets.
+        <p className='they-need-your-love'>
+            Until you have no saved, fostered or adopted pets, take a look at the adoptable ones
             </p>
+        {!(likedPetsUpdated || adoptedPetsUpdated || fosteredPetsUpdated) && (
             <div className='mypets-page-petsfeed-container'>
-              Look for adoptable ones
               <AdoptablePetsFeed />
             </div>
-          </div>
         )}
       </div>
     </div>
