b9a353e5f07b5d0c41f21de4c2bc7d25dfb220ea
Marie Slovokhotov <110108878+marieslo@users.noreply.github.com>
2024-03-02 17:07:25 +0200

    Update MyPetsProvider.jsx

--- src/context/MyPetsProvider.jsx
+++ src/context/MyPetsProvider.jsx
@@ -83,24 +83,16 @@ export default function MyPetsProvider ({ children }) {
     }
   };
 
-  const returnPet = async (petId) => {
-    try {
-      await axios.put(`${SERVER_URL}/pets/${petId}/return`, { userId: user._id });
-      setFosteredPets(prevFosteredPets => prevFosteredPets.filter(id => id !== petId));
-      setAdoptedPets(prevAdoptedPets => prevAdoptedPets.filter(id => id !== petId));
-    } catch (error) {
-      console.error('Error returning pet:', error);
-      setError(error);
-    }
-  };
-
-  // Update adopted and fostered pets when their respective state changes
-  useEffect(() => {
-    const updatedAdoptedPets = adoptedPets.filter(petId => !fosteredPets.includes(petId));
-    const updatedFosteredPets = fosteredPets.filter(petId => !adoptedPets.includes(petId));
-    setAdoptedPets(updatedAdoptedPets);
-    setFosteredPets(updatedFosteredPets);
-  }, [adoptedPets, fosteredPets]);
+const returnPet = async (petId) => {
+  try {
+    await axios.put(`${SERVER_URL}/pets/${petId}/return`, { userId: user._id });
+    setFosteredPets(prevFosteredPets => prevFosteredPets.filter(id => id !== petId));
+    setAdoptedPets(prevAdoptedPets => prevAdoptedPets.filter(id => id !== petId));
+  } catch (error) {
+    console.error('Error returning pet:', error);
+    setError(error);
+  }
+};
 
   return (
     <MyPetsContext.Provider
