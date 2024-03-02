2abd69483abb0c309f7afd21493e798958f87c03
Marie Slovokhotov <110108878+marieslo@users.noreply.github.com>
2024-03-02 21:45:26 +0200

    Update SinglePetPage.jsx

--- src/pages/SinglePetPage/SinglePetPage.jsx
+++ src/pages/SinglePetPage/SinglePetPage.jsx
@@ -12,7 +12,7 @@ import unlikeIcon from '../../styles/icons/heart-outlined.png';
 export default function SinglePetPage() {
   const { id } = useParams();
   const { fetchPetById } = useContext(FetchPetsContext);
-  const { likePet, unlikePet, adoptPet, fosterPet, returnPet } = useMyPetsContext(); 
+  const { likePet, unlikePet, adoptPet, fosterPet, returnPet, adoptedPets, fosteredPets } = useMyPetsContext(); 
   const { user } = useAuth();
 
   const [showAlert, setShowAlert] = useState(false);
