089aad9cb21bd9eaa7fe3a1f519c9fe8ba8bd30a
Marie Slovokhotov <110108878+marieslo@users.noreply.github.com>
2024-03-02 20:09:52 +0200

    Update SinglePetPage.jsx

--- src/pages/SinglePetPage/SinglePetPage.jsx
+++ src/pages/SinglePetPage/SinglePetPage.jsx
@@ -1,21 +1,31 @@
+import React, { useEffect, useState, useContext } from 'react';
+import { Card, Spinner, Alert } from 'react-bootstrap';
+import { useParams } from 'react-router-dom';
+import localforage from 'localforage';
+import './SinglePetPage.css';
+import { FetchPetsContext } from '../../context/FetchPetsProvider';
+import { useMyPetsContext } from '../../context/MyPetsProvider';
+import { useAuth } from '../../context/AuthProvider';
+import likeIcon from '../../styles/icons/heart-filled.png';
+import unlikeIcon from '../../styles/icons/heart-outlined.png';
+
 export default function SinglePetPage() {
   const { id } = useParams();
   const { fetchPetById } = useContext(FetchPetsContext);
-  const { likePet, unlikePet, adoptPet, fosterPet, returnPet, adoptedPets, fosteredPets } = useMyPetsContext(); 
+  const { likePet, unlikePet, adoptPet, fosterPet, returnPet, isCurrentUserAdopterOrFosterer } = useMyPetsContext(); 
   const { user } = useAuth();
 
   const [showAlert, setShowAlert] = useState(false);
   const [loading, setLoading] = useState(true);
   const [petData, setPetData] = useState(null);
   const [isLiked, setIsLiked] = useState(false);
-  const isOwner = petData?.owner === user?.id;
 
   useEffect(() => {
     const fetchPetData = async () => {
       try {
         const data = await fetchPetById(id);
         setPetData(data);
-        const storedLikedStatus = await localforage.getItem(`likedStatus_${user?.id}_${id}`);
+        const storedLikedStatus = await localforage.getItem(`likedStatus_${user?._id}_${id}`);
         setIsLiked(storedLikedStatus || false);
       } catch (error) {
         console.error('Error fetching pet data:', error.message);
@@ -23,9 +33,78 @@ export default function SinglePetPage() {
         setLoading(false);
       }
     };
-
+  
     fetchPetData();
-  }, [fetchPetById, id, user?.id]);
+  }, [fetchPetById, id, user._id]);
+
+  const handleLike = async () => {
+    try {
+      if (!user) {
+        setShowAlert(true);
+      } else {
+        await likePet(id);
+        setIsLiked(true);
+        await localforage.setItem(`likedStatus_${user._id}_${id}`, true);
+      }
+    } catch (error) {
+      console.error('Error liking pet:', error);
+      setShowAlert(true);
+    }
+  };
+
+  const handleUnlike = async () => {
+    try {
+      if (!user) {
+        setShowAlert(true);
+        return;
+      }
+      await unlikePet(id);
+      setIsLiked(false);
+      await localforage.removeItem(`likedStatus_${user._id}_${id}`);
+    } catch (error) {
+      console.error('Error updating liked status:', error);
+      setShowAlert(true);
+    }
+  };
+
+  const handleAdopt = async () => {
+    try {
+      await adoptPet(id);
+      setPetData(prevPetData => ({
+        ...prevPetData,
+        adoptionStatus: 'adopted'
+      }));
+    } catch (error) {
+      console.error('Error adopting pet:', error);
+      setShowAlert(true);
+    }
+  };
+  
+  const handleFoster = async () => {
+    try {
+      await fosterPet(id);
+      setPetData(prevPetData => ({
+        ...prevPetData,
+        adoptionStatus: 'fostered'
+      }));
+    } catch (error) {
+      console.error('Error fostering pet:', error);
+      setShowAlert(true);
+    }
+  };
+  
+  const handleReturn = async () => {
+    try {
+      await returnPet(id);
+      setPetData(prevPetData => ({
+        ...prevPetData,
+        adoptionStatus: 'adoptable'
+      }));
+    } catch (error) {
+      console.error('Error returning pet:', error);
+      setShowAlert(true);
+    }
+  };
 
   if (loading) {
     return <Spinner className='single-pet-page-spinner' animation="grow" variant="light" />;
@@ -85,7 +164,7 @@ export default function SinglePetPage() {
               <Card.Text><u>Breed:</u> {breed}</Card.Text>
             </div>
             <div className="pet-buttons">
-              {adoptionStatus === 'adoptable' && (
+              {(adoptionStatus === 'adoptable') && (
                 <>
                   <button className='pet-page-btn' onClick={handleAdopt}>
                     Adopt
@@ -96,20 +175,13 @@ export default function SinglePetPage() {
                 </>
               )}
               {(adoptionStatus === 'adopted' || adoptionStatus === 'fostered') && (
-                <>
-                  {isOwner ? (
-                    <button className='pet-page-btn' onClick={handleReturn}>
-                      Return
-                    </button>
-                  ) : (
-                    <div className="pet-already-has-home-message">
-                      This pet has already found its home.
-                      <br/>
-                      But its status may change later, so you can save it by clicking Like
-                    </div>
-                  )}
-                </>
-              )}
+                <button
+                  className='pet-page-btn'
+                  onClick={handleReturn}
+                >
+                  Return
+                </button>
+               )}
             </div>
           </Card.Body>
         </Card>
