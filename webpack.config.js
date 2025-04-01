ca14e8d365f5604381424f1ad5cd029326929b80
marieslo <maria.slovohotova@gmail.com>
2025-04-01 14:15:43 +0400

    preeparing for deploy

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,11 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { fileURLToPath } = require('url');
-const { dirname } = require('path');
-
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
 
-module.exports = {
+export default {
   entry: './src/main.jsx',
   module: {
     rules: [
